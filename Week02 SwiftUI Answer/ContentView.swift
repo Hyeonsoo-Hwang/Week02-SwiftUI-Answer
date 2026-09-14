c3ad492a73445fda82204a1a0e74d4a4c94da7a8
Minsu <hunip2002@naver.com>
2026-09-14 15:07:21 +0900

    Initial Commit

---
+++ Week02 SwiftUI Answer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week02 SwiftUI Answer
+//
+//  Created by 공학관559 on 9/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week02 SwiftUI Answer/Week02_SwiftUI_AnswerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week02_SwiftUI_AnswerApp.swift
+//  Week02 SwiftUI Answer
+//
+//  Created by 공학관559 on 9/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Week02_SwiftUI_AnswerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
